944d908d1aa47e39a2020bec9a03b9a27716dc3e
omni360 <omni360@qq.com>
2014-09-10 19:06:25 +0800

    update

--- Three.js
+++ Three.js
@@ -223,17 +223,17 @@ THREE.Color.prototype = {
 	///<returns type="Color">返回颜色对象</returns>
 	set: function ( value ) {
 
-		if ( value instanceof THREE.Color ) {	
+		if ( value instanceof THREE.Color ) {	//如果参数value是THREE.Color的实例化对象
 
-			this.copy( value );
+			this.copy( value );		//调用THREE.copy方法,复制value对象
 
-		} else if ( typeof value === 'number' ) {
+		} else if ( typeof value === 'number' ) {	//如果参数value的类型为number
 
-			this.setHex( value );
+			this.setHex( value );	//调用setHex方法,设置16进制颜色值
 
-		} else if ( typeof value === 'string' ) {
+		} else if ( typeof value === 'string' ) {	//如果参数value的类型为string
 
-			this.setStyle( value );
+			this.setStyle( value );	//调用setRGB方法,设置rgb颜色值
 
 		}
 
@@ -242,8 +242,9 @@ THREE.Color.prototype = {
 	},
 	/*setHex方法
 	///setHex方法用于设置16进制颜色值给当前实例
+	///更多关于hex颜色的内容参考维基百科,http://zh.wikipedia.org/wiki/%E7%BD%91%E9%A1%B5%E9%A2%9C%E8%89%B2
 	*/
-	///summary>setHex</summary>
+	///<summary>setHex</summary>
 	///<param name ="hex" type="number(16进制颜色值0xffddff）">16进制数值0xffddff</param>
 	///<returns type="Color">返回颜色对象</returns>
 	setHex: function ( hex ) {
@@ -258,20 +259,49 @@ THREE.Color.prototype = {
 
 	},
 
+	/*setRGB方法
+	///setRGB方法用于设置rgb(255,255,255)颜色值给当前实例
+	///更多关于rgb颜色的内容参考维基百科,http://zh.wikipedia.org/wiki/%E4%B8%89%E5%8E%9F%E8%89%B2%E5%85%89%E6%A8%A1%E5%BC%8F
+	///或 http://zh.wikipedia.org/wiki/%E7%BD%91%E9%A1%B5%E9%A2%9C%E8%89%B2
+	*/
+	///<summary>setRGB</summary>
+	///<param name ="r" type="number(0-255）">red(0-255)红色</param>
+	///<param name ="g" type="number(0-255）">green(0-255)绿色</param>
+	///<param name ="b" type="number(0-255）">blue(0-255)蓝色</param>
+	///<returns type="Color">返回颜色对象</returns>
 
 	setRGB: function ( r, g, b ) {
 
-		this.r = r;
-		this.g = g;
-		this.b = b;
+		this.r = r;	//将参数r赋值给Color.r
+		this.g = g;	//将参数r赋值给Color.g
+		this.b = b;	//将参数r赋值给Color.b
 
-		return this;
+		return this; //返回颜色对象
 
 	},
+
+	/*setHSL方法
+	///HSL颜色HSL和HSV都是一种将RGB色彩模型中的点在圆柱坐标系中的表示法。这两种表示法试图做到比RGB基于笛卡尔
+	///坐标系的几何结构更加直观.HSL即色相、饱和度、亮度（英语：Hue, Saturation, Lightness）
+	///
+	/// 色相（H）是色彩的基本属性，就是平常所说的颜色名称，如红色、黄色等，取值范围是0-360
+	/// 饱和度（S）是指色彩的纯度，越高色彩越纯，低则逐渐变灰，取0-100%的数值，输入时为0.0-1.0.
+	/// 明度（V），亮度（L），取0-100%，输入时为0.0-1.0.
+	///
+	///更多关于hsl颜色的内容参考维基百科, http://zh.wikipedia.org/wiki/HSL%E5%92%8CHSV%E8%89%B2%E5%BD%A9%E7%A9%BA%E9%97%B4
+	///或者 http://zh.wikipedia.org/wiki/%E7%BD%91%E9%A1%B5%E9%A2%9C%E8%89%B2
+	///setHSL方法用于设置 hsl(360,1.0,0.5)颜色值给当前实例
+	*/
+	///<summary>setHSL</summary>
+	///<param name ="h" type="number(0-360）">hue(0-360)色相</param>
+	///<param name ="s" type="number(0.0-1.0）">saturation(0.0-1.0)饱和度</param>
+	///<param name ="l" type="number(0.0-1.0）">lightness(0.0-1.0)亮度</param>
+	///<returns type="Color">返回颜色对象</returns>
 
 	setHSL: function ( h, s, l ) {
 
 		// h,s,l ranges are in 0.0 - 1.0
+		//h,s,l 取值范围是 0.0 - 1.0
 
 		if ( s === 0 ) {
 
@@ -586,33 +616,180 @@ THREE.Color.prototype = {
 	}
 
 };
+THREE.ColorKeywords = { 
+		'aliceblue': 0xF0F8FF,
+		 'antiquewhite': 0xFAEBD7,
+		 'aqua': 0x00FFFF,
+		 'aquamarine': 0x7FFFD4,
+		 'azure': 0xF0FFFF,
 
-THREE.ColorKeywords = { 'aliceblue': 0xF0F8FF, 'antiquewhite': 0xFAEBD7, 'aqua': 0x00FFFF, 'aquamarine': 0x7FFFD4, 'azure': 0xF0FFFF,
-'beige': 0xF5F5DC, 'bisque': 0xFFE4C4, 'black': 0x000000, 'blanchedalmond': 0xFFEBCD, 'blue': 0x0000FF, 'blueviolet': 0x8A2BE2,
-'brown': 0xA52A2A, 'burlywood': 0xDEB887, 'cadetblue': 0x5F9EA0, 'chartreuse': 0x7FFF00, 'chocolate': 0xD2691E, 'coral': 0xFF7F50,
-'cornflowerblue': 0x6495ED, 'cornsilk': 0xFFF8DC, 'crimson': 0xDC143C, 'cyan': 0x00FFFF, 'darkblue': 0x00008B, 'darkcyan': 0x008B8B,
-'darkgoldenrod': 0xB8860B, 'darkgray': 0xA9A9A9, 'darkgreen': 0x006400, 'darkgrey': 0xA9A9A9, 'darkkhaki': 0xBDB76B, 'darkmagenta': 0x8B008B,
-'darkolivegreen': 0x556B2F, 'darkorange': 0xFF8C00, 'darkorchid': 0x9932CC, 'darkred': 0x8B0000, 'darksalmon': 0xE9967A, 'darkseagreen': 0x8FBC8F,
-'darkslateblue': 0x483D8B, 'darkslategray': 0x2F4F4F, 'darkslategrey': 0x2F4F4F, 'darkturquoise': 0x00CED1, 'darkviolet': 0x9400D3,
-'deeppink': 0xFF1493, 'deepskyblue': 0x00BFFF, 'dimgray': 0x696969, 'dimgrey': 0x696969, 'dodgerblue': 0x1E90FF, 'firebrick': 0xB22222,
-'floralwhite': 0xFFFAF0, 'forestgreen': 0x228B22, 'fuchsia': 0xFF00FF, 'gainsboro': 0xDCDCDC, 'ghostwhite': 0xF8F8FF, 'gold': 0xFFD700,
-'goldenrod': 0xDAA520, 'gray': 0x808080, 'green': 0x008000, 'greenyellow': 0xADFF2F, 'grey': 0x808080, 'honeydew': 0xF0FFF0, 'hotpink': 0xFF69B4,
-'indianred': 0xCD5C5C, 'indigo': 0x4B0082, 'ivory': 0xFFFFF0, 'khaki': 0xF0E68C, 'lavender': 0xE6E6FA, 'lavenderblush': 0xFFF0F5, 'lawngreen': 0x7CFC00,
-'lemonchiffon': 0xFFFACD, 'lightblue': 0xADD8E6, 'lightcoral': 0xF08080, 'lightcyan': 0xE0FFFF, 'lightgoldenrodyellow': 0xFAFAD2, 'lightgray': 0xD3D3D3,
-'lightgreen': 0x90EE90, 'lightgrey': 0xD3D3D3, 'lightpink': 0xFFB6C1, 'lightsalmon': 0xFFA07A, 'lightseagreen': 0x20B2AA, 'lightskyblue': 0x87CEFA,
-'lightslategray': 0x778899, 'lightslategrey': 0x778899, 'lightsteelblue': 0xB0C4DE, 'lightyellow': 0xFFFFE0, 'lime': 0x00FF00, 'limegreen': 0x32CD32,
-'linen': 0xFAF0E6, 'magenta': 0xFF00FF, 'maroon': 0x800000, 'mediumaquamarine': 0x66CDAA, 'mediumblue': 0x0000CD, 'mediumorchid': 0xBA55D3,
-'mediumpurple': 0x9370DB, 'mediumseagreen': 0x3CB371, 'mediumslateblue': 0x7B68EE, 'mediumspringgreen': 0x00FA9A, 'mediumturquoise': 0x48D1CC,
-'mediumvioletred': 0xC71585, 'midnightblue': 0x191970, 'mintcream': 0xF5FFFA, 'mistyrose': 0xFFE4E1, 'moccasin': 0xFFE4B5, 'navajowhite': 0xFFDEAD,
-'navy': 0x000080, 'oldlace': 0xFDF5E6, 'olive': 0x808000, 'olivedrab': 0x6B8E23, 'orange': 0xFFA500, 'orangered': 0xFF4500, 'orchid': 0xDA70D6,
-'palegoldenrod': 0xEEE8AA, 'palegreen': 0x98FB98, 'paleturquoise': 0xAFEEEE, 'palevioletred': 0xDB7093, 'papayawhip': 0xFFEFD5, 'peachpuff': 0xFFDAB9,
-'peru': 0xCD853F, 'pink': 0xFFC0CB, 'plum': 0xDDA0DD, 'powderblue': 0xB0E0E6, 'purple': 0x800080, 'red': 0xFF0000, 'rosybrown': 0xBC8F8F,
-'royalblue': 0x4169E1, 'saddlebrown': 0x8B4513, 'salmon': 0xFA8072, 'sandybrown': 0xF4A460, 'seagreen': 0x2E8B57, 'seashell': 0xFFF5EE,
-'sienna': 0xA0522D, 'silver': 0xC0C0C0, 'skyblue': 0x87CEEB, 'slateblue': 0x6A5ACD, 'slategray': 0x708090, 'slategrey': 0x708090, 'snow': 0xFFFAFA,
-'springgreen': 0x00FF7F, 'steelblue': 0x4682B4, 'tan': 0xD2B48C, 'teal': 0x008080, 'thistle': 0xD8BFD8, 'tomato': 0xFF6347, 'turquoise': 0x40E0D0,
-'violet': 0xEE82EE, 'wheat': 0xF5DEB3, 'white': 0xFFFFFF, 'whitesmoke': 0xF5F5F5, 'yellow': 0xFFFF00, 'yellowgreen': 0x9ACD32 };
+		'beige': 0xF5F5DC,
+		 'bisque': 0xFFE4C4,
+		 'black': 0x000000,
+		 'blanchedalmond': 0xFFEBCD,
+		 'blue': 0x0000FF,
+		 'blueviolet': 0x8A2BE2,
 
-// File:src/math/Quaternion.js
+		'brown': 0xA52A2A,
+		 'burlywood': 0xDEB887,
+		 'cadetblue': 0x5F9EA0,
+		 'chartreuse': 0x7FFF00,
+		 'chocolate': 0xD2691E,
+		 'coral': 0xFF7F50,
+
+		'cornflowerblue': 0x6495ED,
+		 'cornsilk': 0xFFF8DC,
+		 'crimson': 0xDC143C,
+		 'cyan': 0x00FFFF,
+		 'darkblue': 0x00008B,
+		 'darkcyan': 0x008B8B,
+
+		'darkgoldenrod': 0xB8860B,
+		 'darkgray': 0xA9A9A9,
+		 'darkgreen': 0x006400,
+		 'darkgrey': 0xA9A9A9,
+		 'darkkhaki': 0xBDB76B,
+		 'darkmagenta': 0x8B008B,
+
+		'darkolivegreen': 0x556B2F,
+		 'darkorange': 0xFF8C00,
+		 'darkorchid': 0x9932CC,
+		 'darkred': 0x8B0000,
+		 'darksalmon': 0xE9967A,
+		 'darkseagreen': 0x8FBC8F,
+
+		'darkslateblue': 0x483D8B,
+		 'darkslategray': 0x2F4F4F,
+		 'darkslategrey': 0x2F4F4F,
+		 'darkturquoise': 0x00CED1,
+		 'darkviolet': 0x9400D3,
+
+		'deeppink': 0xFF1493,
+		 'deepskyblue': 0x00BFFF,
+		 'dimgray': 0x696969,
+		 'dimgrey': 0x696969,
+		 'dodgerblue': 0x1E90FF,
+		 'firebrick': 0xB22222,
+
+		'floralwhite': 0xFFFAF0,
+		 'forestgreen': 0x228B22,
+		 'fuchsia': 0xFF00FF,
+		 'gainsboro': 0xDCDCDC,
+		 'ghostwhite': 0xF8F8FF,
+		 'gold': 0xFFD700,
+
+		'goldenrod': 0xDAA520,
+		 'gray': 0x808080,
+		 'green': 0x008000,
+		 'greenyellow': 0xADFF2F,
+		 'grey': 0x808080,
+		 'honeydew': 0xF0FFF0,
+		 'hotpink': 0xFF69B4,
+
+		'indianred': 0xCD5C5C,
+		 'indigo': 0x4B0082,
+		 'ivory': 0xFFFFF0,
+		 'khaki': 0xF0E68C,
+		 'lavender': 0xE6E6FA,
+		 'lavenderblush': 0xFFF0F5,
+		 'lawngreen': 0x7CFC00,
+
+		'lemonchiffon': 0xFFFACD,
+		 'lightblue': 0xADD8E6,
+		 'lightcoral': 0xF08080,
+		 'lightcyan': 0xE0FFFF,
+		 'lightgoldenrodyellow': 0xFAFAD2,
+		 'lightgray': 0xD3D3D3,
+
+		'lightgreen': 0x90EE90,
+		 'lightgrey': 0xD3D3D3,
+		 'lightpink': 0xFFB6C1,
+		 'lightsalmon': 0xFFA07A,
+		 'lightseagreen': 0x20B2AA,
+		 'lightskyblue': 0x87CEFA,
+
+		'lightslategray': 0x778899,
+		 'lightslategrey': 0x778899,
+		 'lightsteelblue': 0xB0C4DE,
+		 'lightyellow': 0xFFFFE0,
+		 'lime': 0x00FF00,
+		 'limegreen': 0x32CD32,
+
+		'linen': 0xFAF0E6,
+		 'magenta': 0xFF00FF,
+		 'maroon': 0x800000,
+		 'mediumaquamarine': 0x66CDAA,
+		 'mediumblue': 0x0000CD,
+		 'mediumorchid': 0xBA55D3,
+
+		'mediumpurple': 0x9370DB,
+		 'mediumseagreen': 0x3CB371,
+		 'mediumslateblue': 0x7B68EE,
+		 'mediumspringgreen': 0x00FA9A,
+		 'mediumturquoise': 0x48D1CC,
+
+		'mediumvioletred': 0xC71585,
+		 'midnightblue': 0x191970,
+		 'mintcream': 0xF5FFFA,
+		 'mistyrose': 0xFFE4E1,
+		 'moccasin': 0xFFE4B5,
+		 'navajowhite': 0xFFDEAD,
+
+		'navy': 0x000080,
+		 'oldlace': 0xFDF5E6,
+		 'olive': 0x808000,
+		 'olivedrab': 0x6B8E23,
+		 'orange': 0xFFA500,
+		 'orangered': 0xFF4500,
+		 'orchid': 0xDA70D6,
+
+		'palegoldenrod': 0xEEE8AA,
+		 'palegreen': 0x98FB98,
+		 'paleturquoise': 0xAFEEEE,
+		 'palevioletred': 0xDB7093,
+		 'papayawhip': 0xFFEFD5,
+		 'peachpuff': 0xFFDAB9,
+
+		'peru': 0xCD853F,
+		 'pink': 0xFFC0CB,
+		 'plum': 0xDDA0DD,
+		 'powderblue': 0xB0E0E6,
+		 'purple': 0x800080,
+		 'red': 0xFF0000,
+		 'rosybrown': 0xBC8F8F,
+
+		'royalblue': 0x4169E1,
+		 'saddlebrown': 0x8B4513,
+		 'salmon': 0xFA8072,
+		 'sandybrown': 0xF4A460,
+		 'seagreen': 0x2E8B57,
+		 'seashell': 0xFFF5EE,
+
+		'sienna': 0xA0522D,
+		 'silver': 0xC0C0C0,
+		 'skyblue': 0x87CEEB,
+		 'slateblue': 0x6A5ACD,
+		 'slategray': 0x708090,
+		 'slategrey': 0x708090,
+		 'snow': 0xFFFAFA,
+
+		'springgreen': 0x00FF7F,
+		 'steelblue': 0x4682B4,
+		 'tan': 0xD2B48C,
+		 'teal': 0x008080,
+		 'thistle': 0xD8BFD8,
+		 'tomato': 0xFF6347,
+		 'turquoise': 0x40E0D0,
+
+		'violet': 0xEE82EE,
+		 'wheat': 0xF5DEB3,
+		 'white': 0xFFFFFF,
+		 'whitesmoke': 0xF5F5F5,
+		 'yellow': 0xFFFF00,
+		 'yellowgreen': 0x9ACD32
+		  };
+
+
 
 /**
  * @author mikael emtinger / http://gomo.se/
