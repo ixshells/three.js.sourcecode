3c468a4973cacc18a3beaae394ccbee432288016
Hou Chunlei <omni360@qq.com>
2014-11-18 10:22:08 +0800

    finished Sphere
    finished Sphere

--- Three.js
+++ Three.js
@@ -5998,16 +5998,16 @@ THREE.Box3.prototype = {
 	},
 
 	/*
-	///empty方法用来判断立方体边界的最大值的x,y是否小于最小值的x,y.
+	///empty方法用来判断立方体边界的最大值的x,y,z是否小于最小值的x,y,z.用来判断空间中最大值小于最小值的球体.
 	/// NOTE:如果此框包括零点在其边界返回true。
 	/// NOTE:一个box有最大,最小边界,最大最小边界分别用一个点表示,这个点被两个边界共享.
-	/// TODO:empty方法没有弄明白.什么时候用.
 	*/
 	///<summary>empty</summary>
 	///<returns type="Boolean">返回true 或者 false</returns>
 	empty: function () {
 
 		// this is a more robust check for empty than ( volume <= 0 ) because volume can get positive with two negative axes
+		// 使程序更加稳健,让避免立方体的最大边界小于最小边界.
 
 		return ( this.max.x < this.min.x ) || ( this.max.y < this.min.y ) || ( this.max.z < this.min.z );	//返回true 或者 false
 
@@ -6193,11 +6193,11 @@ THREE.Box3.prototype = {
 	},
 
 	/*
-	///distanceToPoint方法用来获得边界内一点到最小边界,最大边界的长度(box的12条边的长度).
+	///distanceToPoint方法用来获得三维空间内一点point到立方体边界的最小长度.
 	*/
 	///<summary>distanceToPoint</summary>
-	///<param name ="point" type="Vector3">一个边界内的Vector3的三维点坐标</param>
-	///<returns type="Number">返回边界内一点到最小边界,最大边界的长度(box的12条边的长度).</returns>
+	///<param name ="point" type="Vector3">一个三维空间内的Vector3的三维点坐标</param>
+	///<returns type="Number">三维空间内一点point到立方体边界的最小长</returns>
 	distanceToPoint: function () {
 
 		var v1 = new THREE.Vector3();
@@ -6205,7 +6205,7 @@ THREE.Box3.prototype = {
 		return function ( point ) {
 
 			var clampedPoint = v1.copy( point ).clamp( this.min, this.max );
-			return clampedPoint.sub( point ).length();	//返回边界内一点到最小边界,最大边界的长度(box的12条边的长度).
+			return clampedPoint.sub( point ).length();	//返回三维空间内一点point到立方体边界的最小长.
 
 		};
 
@@ -6239,7 +6239,7 @@ THREE.Box3.prototype = {
 	*/
 	///<summary>intersect</summary>
 	///<param name ="box" type="Box3">一个Box3的立方体边界</param>
-	///<returns type="Boolean">返回当前立方体边界和参数box的交集</returns>
+	///<returns type="Box3">返回当前立方体边界和参数box的交集</returns>
 	intersect: function ( box ) {
 
 		this.min.max( box.min );
@@ -8880,26 +8880,53 @@ THREE.Ray.prototype = {
  * @author bhouston / http://exocortex.com
  * @author mrdoob / http://mrdoob.com/
  */
-
+/*
+///Sphere对象的构造函数.用来在三维空间内创建一个球体对象.Sphere对象的功能函数采用
+///定义构造的函数原型对象来实现.
+///
+///	用法: var center = new Vector3(0,0,0),radius = 5; var sphere = new Sphere(center,radius);
+///创建一个圆心是0,0,0半径是5的球体.
+*/
+///<summary>Sphere</summary>
+///<param name ="center" type="Vector3">中心点坐标值</param>
+///<param name ="radius" type="Number">Number球体半径</param>
 THREE.Sphere = function ( center, radius ) {
 
-	this.center = ( center !== undefined ) ? center : new THREE.Vector3();
-	this.radius = ( radius !== undefined ) ? radius : 0;
+	this.center = ( center !== undefined ) ? center : new THREE.Vector3();	//赋值或者初始化center
+	this.radius = ( radius !== undefined ) ? radius : 0;	//赋值或者初始化radius
 
 };
 
+/****************************************
+****下面是Sphere对象提供的功能函数.
+****************************************/
 THREE.Sphere.prototype = {
 
-	constructor: THREE.Sphere,
+	constructor: THREE.Sphere,	//构造器,返回对创建此对象的Sphere函数的引用
 
+	/*
+	///set方法用来从新设置球体的起始点,结束点,center,radius坐标值.并返回新半径,坐标值的球体.
+	*/
+	///<summary>set</summary>
+	///<param name ="center" type="Vector3">中心点坐标值</param>
+	///<param name ="radius" type="Number">Number球体半径</param>
+	///<returns type="Sphere">返回新半径,坐标值的球体</returns>
 	set: function ( center, radius ) {
 
 		this.center.copy( center );
 		this.radius = radius;
 
-		return this;
+		return this;		//返回新半径,坐标值的球体
 	},
 
+	/*
+	///setFromPoints方法通过获得Vector3对象组成的points数组中的到圆心距离最大的值重新设置球体的半径,通过可选参数optionalCenter用来设置球体的圆心.并返回新半径,坐标值的球体.
+	/// NOTE:注意如果给setFromPoints()方法设置了optionalCenter参数,points数组中数值到圆心的距离将会改变.
+	*/
+	///<summary>setFromPoints</summary>
+	///<param name ="points" type="Vector3Array">Vector3对象组成的points数组</param>
+	///<param name ="optionalCenter" type="Vector3">可选参数,接收返回结果,球体的中心点</param>
+	///<returns type="Sphere">返回新半径,坐标值的球体</returns>
 	setFromPoints: function () {
 
 		var box = new THREE.Box3();
@@ -8922,53 +8949,89 @@ THREE.Sphere.prototype = {
 
 			for ( var i = 0, il = points.length; i < il; i ++ ) {
 
-				maxRadiusSq = Math.max( maxRadiusSq, center.distanceToSquared( points[ i ] ) );
+				maxRadiusSq = Math.max( maxRadiusSq, center.distanceToSquared( points[ i ] ) );	//求points数组中到圆心的最大值并赋值给maxRadiusSq
 
 			}
 
-			this.radius = Math.sqrt( maxRadiusSq );
+			this.radius = Math.sqrt( maxRadiusSq );	
 
-			return this;
+			return this;	//返回新半径,坐标值的球体
 
  		};
 
 	}(),
 
+	/*
+	///copy方法用来复制球体的圆心,半径,center,radius值.返回新半径,坐标值的球体
+	*/
+	///<summary>copy</summary>
+	///<param name ="sphere" type="Sphere">球体</param>
+	///<returns type="Sphere">返回新半径,坐标值的球体</returns>
 	copy: function ( sphere ) {
 
 		this.center.copy( sphere.center );
 		this.radius = sphere.radius;
 
-		return this;
+		return this;	//返回新半径,坐标值的球体
 
 	},
 
+	/*
+	///empty方法用来判断球体的半径是否小于等于0,用来判断空间中半径是0,或者小于0的球体.
+	*/
+	///<summary>empty</summary>
+	///<returns type="Boolean">返回true 或者 false</returns>
 	empty: function () {
 
-		return ( this.radius <= 0 );
+		return ( this.radius <= 0 );	//返回true 或者 false
 
 	},
 
+	/*
+	///containsPoint方法用来获得参数point(一个Vector3的三维点坐标)是否在当前球体内.
+	*/
+	///<summary>containsPoint</summary>
+	///<param name ="point" type="Vector3">一个Vector3的三维点坐标</param>
+	///<returns type="Boolean">返回true 或者 false</returns>
 	containsPoint: function ( point ) {
 
-		return ( point.distanceToSquared( this.center ) <= ( this.radius * this.radius ) );
+		return ( point.distanceToSquared( this.center ) <= ( this.radius * this.radius ) );	//返回true 或者 false
 
 	},
 
+	/*
+	///distanceToPoint方法用来获得三维空间内一点到Sphere球体对象表面的最小长度.
+	*/
+	///<summary>distanceToPoint</summary>
+	///<param name ="point" type="Vector3">一个三维空间内的Vector3的三维点坐标</param>
+	///<returns type="Number">返回三维空间内一点到Sphere球体对象表面的最小长度.</returns>
 	distanceToPoint: function ( point ) {
 
-		return ( point.distanceTo( this.center ) - this.radius );
+		return ( point.distanceTo( this.center ) - this.radius );	//返回三维空间内一点到Sphere球体对象表面的最小长度.
 
 	},
 
+	/*
+	///intersectsSphere方法获取当前球体是否与参数sphere球体对象相交,返回true 或者 false
+	*/
+	///<summary>intersectsSphere</summary>
+	///<param name ="sphere" type="Sphere">一个Sphere的球体</param>
+	///<returns type="Boolean">返回true 或者 false</returns>
 	intersectsSphere: function ( sphere ) {
 
 		var radiusSum = this.radius + sphere.radius;
 
-		return sphere.center.distanceToSquared( this.center ) <= ( radiusSum * radiusSum );
+		return sphere.center.distanceToSquared( this.center ) <= ( radiusSum * radiusSum );	//返回true 或者 false
 
 	},
 
+	/*
+	///clampPoint方法用来通过参数point收缩球体.如果point在球体外,强制将point设置到球体表面,如果point在球体内,重新设置球体半径为point到当前球体半径的距离.
+	*/
+	///<summary>clampPoint</summary>
+	///<param name ="point" type="Vector3">一个Vector3的三维点坐标</param>
+	///<param name ="optionalTarget" type="Vector3">可选参数,接收返回结果,返回剪裁过的边界点</param>
+	///<returns type="Vector3">返回剪裁过的边界点.</returns>
 	clampPoint: function ( point, optionalTarget ) {
 
 		var deltaLengthSq = this.center.distanceToSquared( point );
@@ -8983,10 +9046,17 @@ THREE.Sphere.prototype = {
 
 		}
 
-		return result;
+		return result;	// 返回剪裁过的边界点
 
 	},
 
+	/*
+	///getBoundingBox方法返回当前球体的Box3立方体边界(这里应该外切于球体的一个立方体)
+	/// 与Box3类中的getBoundingSphere()方法对应.
+	*/
+	///<summary>getBoundingBox</summary>
+	///<param name ="optionalTarget" type="THREE.Box3()">可选参数,THREE.Box3()立方体对象,用来接收返回值</param>
+	///<returns type="THREE.Sphere()">返回当前球体的Box3立方体边界(这里应该外切于球体的一个立方体)</returns>
 	getBoundingBox: function ( optionalTarget ) {
 
 		var box = optionalTarget || new THREE.Box3();
@@ -8994,36 +9064,59 @@ THREE.Sphere.prototype = {
 		box.set( this.center, this.center );
 		box.expandByScalar( this.radius );
 
-		return box;
+		return box;			//返回当前球体的Box3立方体边界(这里应该外切于球体的一个立方体)
 
 	},
 
+	/*
+	///applyMatrix4方法通过传递matrix(旋转,缩放,移动等变换矩阵)对当前Sphere球体对象的圆心和半径,应用变换.
+	*/
+	///<summary>applyMatrix4</summary>
+	///<param name ="matrix" type="Matrix4">(旋转,缩放,移动等变换矩阵</param>
+	///<returns type="Boolean">返回变换后的球体.</returns>
 	applyMatrix4: function ( matrix ) {
 
 		this.center.applyMatrix4( matrix );
 		this.radius = this.radius * matrix.getMaxScaleOnAxis();
 
-		return this;
+		return this;		//返回变换后的球体.
 
 	},
 
+	/*
+	///translate方法用来通过参数offset,移动当前球体的位置.
+	*/
+	///<summary>translate</summary>
+	///<param name ="offset" type="Vector3">偏移量</param>
+	///<returns type="Boolean">返回新半径,坐标值的球体</returns>
 	translate: function ( offset ) {
 
 		this.center.add( offset );
 
-		return this;
+		return this;	//返回新半径,坐标值的球体
 
 	},
 
+	/*
+	///equals方法用来获得参数sphere(一个Sphere的球体)是否与当前球体完全相等,即圆心和半径相等.
+	*/
+	///<summary>equals</summary>
+	///<param name ="sphere" type="Sphere">一个Sphere的球体</param>
+	///<returns type="Boolean">返回true 或者 false</returns>
 	equals: function ( sphere ) {
 
-		return sphere.center.equals( this.center ) && ( sphere.radius === this.radius );
+		return sphere.center.equals( this.center ) && ( sphere.radius === this.radius );	//返回true 或者 false
 
 	},
 
+	/*clone方法
+	///clone方法克隆一个球体对象.
+	*/
+	///<summary>clone</summary>
+	///<returns type="Sphere">返回球体对象</returns>	
 	clone: function () {
 
-		return new THREE.Sphere().copy( this );
+		return new THREE.Sphere().copy( this );		//返回球体对象
 
 	}
 
